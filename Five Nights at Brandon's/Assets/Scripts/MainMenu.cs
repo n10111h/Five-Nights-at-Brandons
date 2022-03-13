2f6556a2fa5e28d359b92123c6cb997d06aa46b5
n10111h <njsl10111@gmail.com>
2022-03-12 22:57:03 -0800

    Beta 0.2

---
+++ Five Nights at Brandon's/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    public TMP_Text text;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        text.fontSize = 12;
+        text.text = "No escape, only way out is Brandon";
+    }
+}
