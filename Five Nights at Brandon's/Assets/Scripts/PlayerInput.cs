19817b1ab0678d040eb3f54359cb86af74f7818f
n10111h <njsl10111@gmail.com>
2022-03-13 00:01:44 -0800

    idk man im tired

--- Five Nights at Brandon's/Assets/Scripts/PlayerInput.cs
+++ Five Nights at Brandon's/Assets/Scripts/PlayerInput.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerInput : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
-    void Update()
+    float AngleAboutY(Transform obj)
     {
-        
+        Vector3 objFwd = obj.forward;
+        float angle = Vector3.Angle(objFwd, Vector3.forward);
+        float sign = Mathf.Sign(Vector3.Cross(objFwd, Vector3.forward).y);
+        return angle * sign;
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (AngleAboutY(this.transform) > 130 || AngleAboutY(this.transform) < 0) transform.Rotate(0f, 1f, 0f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (AngleAboutY(this.transform) < -130 || AngleAboutY(this.transform) > 0) transform.Rotate(0f, -1f, 0f, Space.World);
+        }
     }
 }
