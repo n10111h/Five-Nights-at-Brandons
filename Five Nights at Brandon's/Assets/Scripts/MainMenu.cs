b2cac95c33023c765848fa2604a8c09a0f5013ff
n10111h <njsl10111@gmail.com>
2022-03-15 11:28:04 -0700

    beta 0.3

--- Five Nights at Brandon's/Assets/Scripts/MainMenu.cs
+++ Five Nights at Brandon's/Assets/Scripts/MainMenu.cs
@@ -7,6 +7,7 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
     public TMP_Text text;
+    public TMP_Text direction;
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
@@ -15,5 +16,10 @@ public class MainMenu : MonoBehaviour
     {
         text.fontSize = 12;
         text.text = "No escape, only way out is Brandon";
+    }
+    public void HowTo()
+    {
+        direction.fontSize = 12;
+        direction.text = "A and D to move your view. Q and E to toggle doors.\nSpace to toggle Cams. Click on cam UI for respective cams.\n Other than that just survive idk.";
     }
 }
