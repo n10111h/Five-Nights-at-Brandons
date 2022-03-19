82c7b2b490e1b84dc620e93075c8df91ae729d85
n10111h <njsl10111@gmail.com>
2022-03-18 20:07:42 -0700

    position manager for animatronics

--- Five Nights at Brandon's/Assets/Scripts/Bonnie.cs
+++ Five Nights at Brandon's/Assets/Scripts/Bonnie.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Freddy : MonoBehaviour
+public class Bonnie : MonoBehaviour
 {
     public GameObject powerUI;
     private float power;
-    public GameObject player;
     private float time;
     public short level;
     private int pos;
     void Start()
     {
         time = Time.time;
+        pos = 0;
     }
 
     // Update is called once per frame
@@ -21,7 +21,7 @@ public class Freddy : MonoBehaviour
         power = powerUI.GetComponent<power>().powerPercent;
         if (power>0)
         {
-
+            
         }
         else
         {
@@ -32,15 +32,8 @@ public class Freddy : MonoBehaviour
     {
         if (power < 0)
         {
-            if (Time.time - time > 15)
-            {
-                time = Time.time;
-            }
-            transform.position = new Vector3(0.145f, 0.553f, 6.62f);
-            transform.rotation = Quaternion.Euler(90, 90, 90);
-            if (Time.time - time > 10) Application.Quit();
+            
         }
-
         else
         {
             float rand = Random.Range(0.0f, 999.0f);
--- Five Nights at Brandon's/Assets/Scripts/Chica.cs
+++ Five Nights at Brandon's/Assets/Scripts/Chica.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Freddy : MonoBehaviour
+public class Chica : MonoBehaviour
 {
     public GameObject powerUI;
     private float power;
-    public GameObject player;
     private float time;
     public short level;
     private int pos;
@@ -32,15 +31,8 @@ public class Freddy : MonoBehaviour
     {
         if (power < 0)
         {
-            if (Time.time - time > 15)
-            {
-                time = Time.time;
-            }
-            transform.position = new Vector3(0.145f, 0.553f, 6.62f);
-            transform.rotation = Quaternion.Euler(90, 90, 90);
-            if (Time.time - time > 10) Application.Quit();
+            
         }
-
         else
         {
             float rand = Random.Range(0.0f, 999.0f);
