091ffe226ad472c01527f37860d1ed5a7b16cc52
n10111h <njsl10111@gmail.com>
2022-03-18 22:29:01 -0700

    Bonnie Movement

--- Five Nights at Brandon's/Assets/Scripts/Bonnie.cs
+++ Five Nights at Brandon's/Assets/Scripts/Bonnie.cs
@@ -9,6 +9,39 @@ public class Bonnie : MonoBehaviour
     private float time;
     public short level;
     private int pos;
+    void posUpdate()
+    {
+        switch (pos)
+        {
+            case 0:
+                transform.position = new Vector3(0.623f, 1.087f, -5.518f);
+                transform.rotation = Quaternion.Euler(90,130,90);
+                break;
+            case 1:
+                transform.position = new Vector3(4.777f, 0.873f, -3.446f);
+                transform.rotation = Quaternion.Euler(100, 130, -180);
+                break;
+            case 2:
+                transform.position = new Vector3(-0.209f, 0.867f, -3.364f);
+                transform.rotation = Quaternion.Euler(90, 180, -50);
+                break;
+            case 3:
+                transform.position = new Vector3(2.289f, 0.535f, 2.558f);
+                transform.rotation = Quaternion.Euler(90, 0, 90);
+                break;
+            case 4:
+                transform.position = new Vector3(1.265f, 0.535f, 4.554f);
+                transform.rotation = Quaternion.Euler(75, -55, 0);
+                break;
+            case 5:
+                transform.position = new Vector3(1.429f, 0.535f, 5.587f);
+                transform.rotation = Quaternion.Euler(90, -55, 0);
+                break;
+            default:
+                break;
+
+        }
+    }
     void Start()
     {
         time = Time.time;
@@ -39,5 +72,6 @@ public class Bonnie : MonoBehaviour
             float rand = Random.Range(0.0f, 999.0f);
             if (rand < level) pos++;
         }
+        posUpdate();
     }
 }
